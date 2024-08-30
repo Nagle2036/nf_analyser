900c2d462bed163a091d0a49c2bca2f3f5ce2102
Nagle2036 <alexnagle@live.co.uk>
2024-08-30 16:17:55 +0100

    use aroma

--- mri_processor.py
+++ mri_processor.py
@@ -1518,7 +1518,7 @@ singularity run --cleanenv \
         --skip_bids_validation \
         --participant-label ${SUBJECT} \
         --omp-nthreads 5 --nthreads 5 --mem_mb 30000 \
-        --low-mem --use-aroma\
+        --low-mem --use-aroma True \
         --output-spaces MNI152NLin2009cAsym:res-2 \
         --fs-license-file /license \
         --work-dir /wd \

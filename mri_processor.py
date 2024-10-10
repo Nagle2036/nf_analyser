87edca65d0eb8798353ebccbf6dea06a136f917f
Nagle2036 <alexnagle@live.co.uk>
2024-10-10 11:12:44 +0100

    resampling again

--- mri_processor.py
+++ mri_processor.py
@@ -3214,31 +3214,27 @@ set fmri(overwrite_yn) 0
     reshaped_roi_path = 'data/roi/SCCsphere8_bin_2mm_reshaped.nii.gz'  
     
     img = nib.load(raw_roi_path)
-    img.header['qform_code'] = 4
-    qto_xyz = img.header.get_qform()
-    qto_xyz[0, 3] = -90.0
-    img.set_qform(qto_xyz)
-    img.header['sform_code'] = 4
-    sto_xyz = img.header.get_sform()
-    sto_xyz[0, 3] = -90.0
-    img.set_sform(sto_xyz)
-    nib.save(img, 'data/roi/SCCsphere8_bin_2mm_reshaped.nii.gz')
-
-
-    img = nib.load(reshaped_roi_path)
-    original_shape = img.shape
     original_affine = img.affine
     new_shape = (97, 115, 97)
     target_affine = np.copy(original_affine)
     target_affine[0, 0] = 2.0
     target_affine[1, 1] = 2.0
     target_affine[2, 2] = 2.0
-    target_shape = new_shape
-    resampled_img = resample_from_to(img, (target_shape, target_affine))
-    nib.save(resampled_img, 'data/roi/SCCsphere8_bin_2mm_reshaped_final.nii.gz')
-    subprocess.run(['fslmaths', 'data/roi/SCCsphere8_bin_2mm_reshaped_final.nii.gz', '-thr', '0.00000047', '-bin', 'data/roi/SCCsphere8_bin_2mm_reshaped_final.nii.gz'])
+    target_affine[0, 3] = -90.0
+    resampled_img = resample_from_to(img, (new_shape, target_affine))
+    nib.save(resampled_img, reshaped_roi_path)
+    subprocess.run(['fslmaths', reshaped_roi_path, '-thr', '0.00000047', '-bin', reshaped_roi_path])
 
-    
+    # img = nib.load(reshaped_roi_path)
+    # img.header['qform_code'] = 4
+    # qto_xyz = img.header.get_qform()
+    # qto_xyz[0, 3] = -90.0
+    # img.set_qform(qto_xyz)
+    # img.header['sform_code'] = 4
+    # sto_xyz = img.header.get_sform()
+    # sto_xyz[0, 3] = -90.0
+    # img.set_sform(sto_xyz)
+    # nib.save(img, 'data/roi/SCCsphere8_bin_2mm_reshaped_final.nii.gz')
     
 
 
